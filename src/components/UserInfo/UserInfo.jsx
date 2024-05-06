f705c6035628c88bb26351a42acb900de86bb03a
Andres <andresdavidpachecocuadro@gmail.com>
2024-05-05 21:17:39 -0500

    se creó la pagina del perfil de usuario

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import './UserInfo.css';
-import ubicacion from './Location.svg'
+import LocationIcon from '../icons/LocationIcon'
+import "./UserInfo.css";
 
 export const UserInfo = () => {
   return (
-    <section class="flex items-center justify-center h-screen">
-    <img src="https://thispersondoesnotexist.com/" alt="User-Profile-Photo" class="User-photo"/>
-    <div class="ml-4">
-      <h2 class="text-xl font-semibold text-gray-900">ALEX G</h2>
-      <h4 class="mt-1 mb-1">Lorem ipsum dolor sit amet consectetur <br/> adipisicing elit.</h4>
-      <img src={ubicacion} alt="Location Icon" class="Location-icon" /> 
-    </div>
-  </section> 
+    <section className="user-info-container">
+      <img
+        src="https://thispersondoesnotexist.com/"
+        alt="User-Profile-Photo"
+        className="User-photo"
+      />
+      <div className="ml-4">
+        <h2 className="text-xl font-semibold text-gray-900">ALEX G</h2>
+        <h4 className="mt-1 mb-1">
+          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+        </h4>
+        <LocationIcon/>
+      </div>
+    </section>
   );
 };
