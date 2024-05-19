a0ea83deffced907846db3f9bb028e3ff6bae26f
Emilton Navas <emilton.navas@unicolombo.edu.co>
2024-05-19 12:34:41 -0500

    Test componente NotFound-Page PASS

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
  test:{
   globals:true,
+  environment: 'jsdom',
+  setupFiles: './src/setupTests.js'
  },
   plugins: [react()],
 })
