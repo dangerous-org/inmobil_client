480e4328eeae93337a3492a3fa869d1e21389749
Emilton Navas <emilton.navas@unicolombo.edu.co>
2024-05-18 23:14:17 -0500

    tests

---
+++ src/test/Feed.test.jsx
@@ -0,0 +1,20 @@
+// import { render, screen } from '@testing-library/react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import FeedPage from "../../src/pages/feed/Feed-Page";
+
+
+// test('Verifica el rederizado del feed', () => {
+  
+//     render(
+//       <Router>
+//         <FeedPage />
+//       </Router>
+//     );
+  
+    
+//     const linkElement = screen.getByText(/ /i);
+  
+    
+//     expect(linkElement).toBeInTheDocument();
+//   });
+  
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ test:{
+  globals:true,
+ },
   plugins: [react()],
 })
